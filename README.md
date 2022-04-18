794fa796fe44b5cd6aae6eb2e394df22ddbc7d40
Bushra Aboubida Ahmed <bushragtv@gmail.com>
2022-04-18 22:48:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cars-panorama
+ View a photo of cars in panoramic images (360 panorama).  You can open a any image file to view from your computer or this webapp is designed to view 360 panorama mood.
---
+++ main.js
@@ -0,0 +1,30 @@
+// pannellum.viewer('panorama', {
+//     "type": "equirectangular",
+//     "panorama": "cars.jpg",
+//   "autoLoad": true,
+//   "compass": true,
+//   "pitch": -10,
+//   "maxPitch":-10,
+//   "title": "By Omer Mahdi",
+//   "hotSpots": [
+//     {
+//         "pitch": 14.1,
+//         "yaw": 1.5,
+//         "type": "info",
+//         "text": "Baltimore Museum of Art",
+//         "URL": "https://artbma.org/"
+//     },
+//     {
+//         "pitch": -9.4,
+//         "yaw": 222.6,
+//         "type": "info",
+//         "text": "Art Museum Drive"
+//     },
+//     {
+//         "pitch": -0.9,
+//         "yaw": 144.4,
+//         "type": "info",
+//         "text": "North Charles Street"
+//     }
+// ]
+// });
